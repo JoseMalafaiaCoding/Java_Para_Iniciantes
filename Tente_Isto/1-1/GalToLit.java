ce5a5dd37f0912c7121874babc4a0d9f18a185c7
Jose Carlos Malafaia Ferreira Júnior <71335437+JoseMalafaiaCoding@users.noreply.github.com>
2023-02-09 11:16:39 -0300

    Update GalToLit.java

--- Tente_Isto/1-1/GalToLit.java
+++ Tente_Isto/1-1/GalToLit.java
@@ -3,6 +3,9 @@ class GalToLit{
 		double galoes;
 		System.out.println("Insira o valor de galoes a ser convertido:");
 		galoes = 10.0;
+		if(galoes < 2){
+			System.out.println("Valor muito baixo");
+		}
 		System.out.println(galoes + "galoes equivale a " + galoes * 3.7854 + " litros.");
 	}
 }
